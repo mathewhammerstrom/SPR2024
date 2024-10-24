a96d892887688f89b31bf24643f69eed99b40809
mathewhammerstrom <50024894+mathewhammerstrom@users.noreply.github.com>
2024-10-24 02:34:31 -0700

    Delete showCards.m

--- showCards.m
+++
@@ -1,9 +0,0 @@
-function  showCards(win,dstRects,cardTextures,hiddenText)
-
-
-cardTextures(:,4) = Screen('MakeTexture', win, hiddenText);
-Screen('DrawTextures', win, cardTextures, [], dstRects);
-DrawFormattedText(win,'+','center','center',[128 128 128]);
-Screen(win,'TextSize',25);
-Screen('Flip',win);
-WaitSecs(2);
